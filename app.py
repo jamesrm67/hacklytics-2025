4050b8fd1be7b1d74c56063ec93b346cd17809be
thekevinhu <22kevinhu@gmail.com>
2025-02-22 01:30:20 -0500

    Add dream image generation and analysis features with new HTML interface

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-from flask import Flask, request, send_file
-from diffusers import StableDiffusionPipeline
+from flask import Flask, request, send_file, render_template
 from nlp import analyze_dream
+from image_gen import generate_dream_image
 import io
 
 app = Flask(__name__)
 
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id)
-
-def generate_dream_image(dream_prompt):
-    image = pipe(dream_prompt).images[0]
-    return image
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
+    return render_template("index.html")
+
+@app.route("/generate_img", methods=["POST"])
+def generate_img():
     if request.method == "POST":
         dream_prompt = request.form["dream_prompt"]
         image = generate_dream_image(dream_prompt)
@@ -21,12 +18,6 @@ def index():
         image.save(img_io, 'PNG')
         img_io.seek(0)
         return send_file(img_io, mimetype='image/png')
-    return """
-    <form method="post">
-        <input type="text" name="dream_prompt" placeholder="Enter your dream...">
-        <button type="submit">Generate Image</button>
-    </form>
-    """
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
