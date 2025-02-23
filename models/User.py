7385f74b5bfea86f910f75521848f3565dba63d5
thekevinhu <22kevinhu@gmail.com>
2025-02-23 02:23:11 -0500

    Implement user dream analysis and storage; add user retrieval function

--- models/User.py
+++ models/User.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from firebase_admin import auth
 
 class User(UserMixin):
     def __init__(self, id, username, email):
@@ -8,3 +9,12 @@ class User(UserMixin):
     
     def get_id(self):
         return str(self.id)
+
+def get_user(user_id):
+    try:
+        user = auth.get_user(user_id)
+        return User(user.id, user.username, user.email)
+    except auth.UserNotFoundError:
+        return None
+    except Exception as e:
+        return None
